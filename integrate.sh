5dabaa48a50ea104de74964a4f59bdbda6883905
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2011-03-17 15:57:12 +0000

    starting to implement specialization, following this tutorial: http://xiruss.org/tutorials/dita-specialization/dita-specialization-tutorial.pdf
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/dita-prototypes@22696 5cd652fc-3c8e-420e-a729-68eecf2ccb59

---
+++ integrate.sh
@@ -0,0 +1,4 @@
+rm -rf /Users/jorahood/Downloads/oxygen/frameworks/dita/DITA-OT/plugins/org.kuali.kits.kms.doctypes
+cp -r org.kuali.kits.kms.doctypes /Users/jorahood/Downloads/oxygen/frameworks/dita/DITA-OT/plugins/
+cd /Users/jorahood/Downloads/oxygen/frameworks/dita/DITA-OT/
+ant -f integrator.xml
