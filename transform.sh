c35af2f0bc582bca2853d335a4ffa1fd05e8d95a
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2011-03-28 15:56:04 +0000

    dmm3 work
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/dita-prototypes@22709 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- transform.sh
+++ transform.sh
@@ -1,1 +1,1 @@
-ant -Dkb.domain='all' -Dkb.filters='client-ose' -Dargs.input='axvp.ditamap' -f 'build-kb.xml' -q
+ant -Dkb.domain='all' -Dkb.filters='client-ose'  -Dargs.subdir='$1' -Dargs.input='$2' -f 'build-kb.xml' -q
