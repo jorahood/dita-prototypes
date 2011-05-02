1e00a4463a79a15e04dafcfc55dea56ea65e09c7
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2011-05-02 19:08:56 +0000

    Update integrate.sh to point to new location of Oxygen's Open Toolkit plugin dir
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/dita-prototypes@22758 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- integrate.sh
+++ integrate.sh
@@ -1,4 +1,4 @@
-rm -rf /Users/jorahood/Downloads/oxygen/frameworks/dita/DITA-OT/plugins/org.kuali.kits.kms.doctypes
-cp -r org.kuali.kits.kms.doctypes /Users/jorahood/Downloads/oxygen/frameworks/dita/DITA-OT/plugins/
-cd /Users/jorahood/Downloads/oxygen/frameworks/dita/DITA-OT/
+rm -rf /Applications/Oxygen/frameworks/dita/DITA-OT/plugins/org.kuali.kits.kms.doctypes
+cp -r org.kuali.kits.kms.doctypes /Applications/Oxygen/frameworks/dita/DITA-OT/plugins/
+cd /Applications/Oxygen/frameworks/dita/DITA-OT/
 ant -f integrator.xml
