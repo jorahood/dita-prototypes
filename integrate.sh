f1f290a1b01c288fae7d741636a7b482399f2569
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2011-05-02 19:08:59 +0000

    something went wrong
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/dita-prototypes@22759 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- integrate.sh
+++ integrate.sh
@@ -1,4 +1,4 @@
-rm -rf /Applications/Oxygen/frameworks/dita/DITA-OT/plugins/org.kuali.kits.kms.doctypes
-cp -r org.kuali.kits.kms.doctypes /Applications/Oxygen/frameworks/dita/DITA-OT/plugins/
-cd /Applications/Oxygen/frameworks/dita/DITA-OT/
+rm -rf /Applications/Oxygen/frameworks/dita/DITA-OT/plugins/org.kuali.kits.kms &&\
+cp -r org.kuali.kits.kms /Applications/Oxygen/frameworks/dita/DITA-OT/plugins/ &&\
+cd /Applications/Oxygen/frameworks/dita/DITA-OT/ &&\
 ant -f integrator.xml
