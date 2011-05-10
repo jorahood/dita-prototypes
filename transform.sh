ef46696bc104b77938369de1071c2363a5b7b5eb
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2011-05-10 00:16:00 +0000

    made build-kb work with multiple subdirs. transform.sh runs ant with default args for now until can get it working correctly
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/dita-prototypes@22779 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- transform.sh
+++ transform.sh
@@ -1,1 +1,7 @@
-ant -Dkb.domain='all' -Dkb.filters='client-ose'  -Dargs.subdir='$1' -Dargs.input='$2' -f 'build-kb.xml' -q
+#if [ $# -ne 4 ]
+#then
+#    echo "Syntax: transform.sh <kb-domain> <kb-audience> <dmmX-subdir> <input-file>"
+#    exit
+#fi
+
+ant -Dkb.domain=all -Dkb.audience=default  -Dargs.dmm=dmm3 -Dargs.input_file=axvp.ditamap -f 'build-kb.xml' #-q
